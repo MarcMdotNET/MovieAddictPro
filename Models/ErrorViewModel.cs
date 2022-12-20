92f3833a2646b8584f873dd5e5a37e44baa1de8a
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2022-12-20 14:38:09 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieAddictPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
